fec446d9211b0b52e6a6900b336c66d837300d1d
sung-ilkim <skim88.1942@gmail.com>
2021-08-07 17:04:22 +0900

    exerciesing nextjs tutorials about css and styling

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,21 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function index() {
   return (
-  <h1 className="title">
-    Read{' '}
-    <Link href="/posts/first-post">
-      <a>
-        This Page!
-      </a>
-    </Link>
-  </h1>
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1 className="title">
+        Read{' '}
+        <Link href="/posts/first-post">
+          <a>
+            This Page!
+          </a>
+        </Link>
+      </h1>
+    </>
   );
 }
